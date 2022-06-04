157cd8b1048fbe2d9518a4cde1676a7f8855b87e
chihyung.park <chihyung0@gmail.com>
2022-06-04 23:59:33 +0900

    Day 17, adding more Controller in Interface Builder/ having Outlet Connection @IBOutlet

---
+++ Project11/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  Project11
+//
+//  Created by chihyung park on 2022/06/04.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Project11/README.md
+++ Project11/README.md
@@ -4,3 +4,6 @@
 - trying at the first place, The list of picture file didn't come without the setting of Customer Class: View Controller in the Main.storyboard. 
 - not so sure, whether malfunctioning is related to the setting which somehow was missed, "Is initial ViewController".
 
+## Day 17: Issue2: The assistant editor couldn't find any, "No Assistant Result"
+- no reason defined, but as a workaround, by clicking "new windows" button under the big plus button on the right top. 
+
