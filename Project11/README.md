5fbbe0a74b2a4134036d8d716c32f4d5acb8d74c
chihyung.park <chihyung0@gmail.com>
2022-06-04 16:07:12 +0900

    Day 16, print picture files' list on UITableViewController

---
+++ Project11/README.md
@@ -0,0 +1,6 @@
+# Project 1 in UIKit (Day 16 to 22)
+
+## Day 16: Issue1: The list of picture didn't come
+- trying at the first place, The list of picture file didn't come without the setting of Customer Class: View Controller in the Main.storyboard. 
+- not so sure, whether malfunctioning is related to the setting which somehow was missed, "Is initial ViewController".
+
--- Project11/ViewController.swift
+++ Project11/ViewController.swift
@@ -5,15 +5,34 @@
 //  Created by chihyung park on 2022/06/04.
 //
 
-import UIKit
+import UIKit // this file will reference the ios user interface kit.
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    var pictures = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        print(pictures)
     }
-
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
 }
 
